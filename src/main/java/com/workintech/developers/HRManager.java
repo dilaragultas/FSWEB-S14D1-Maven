453b172e7cb06cbe40e5197b4195963930e8341d
dilaragultas <dilaragultas@gmail.com>
2025-07-07 23:03:17 +0200

    07.07.2025
    Completed

---
+++ src/main/java/com/workintech/developers/HRManager.java
@@ -0,0 +1,19 @@
+package com.workintech.developers;
+
+public class HRManager extends Employee{
+
+    public HRManager(int id, String name, double salary) {
+        super(id, name, salary);
+    }
+
+public void work(){
+    System.out.println("HR Manager starts to working");
+}
+    private String[] juniorDevelopers;
+    private String[] midDevelopers;
+    private String[] seniorDevelopers;
+
+    public void addEmployee(){
+
+    }
+}
